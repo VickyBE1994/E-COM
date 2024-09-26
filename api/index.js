ecbb415e356583ded448ea84761e4b553e8b40be
VickyBE1994 <vickyvip81@gmail.com>
2024-09-26 10:58:13 +0530

    order product removed

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/OrderRoute.js'
+
 
 
 //config
